9ca529ed840c4807cd6cad472334e4ac892ebe01
JJ <jjisnow@gmail.com>
2018-07-04 17:22:57 +1000

    replace print statements with logging calls

--- pytube/streams.py
+++ pytube/streams.py
@@ -19,8 +19,8 @@ from pytube import request
 from pytube.helpers import safe_filename
 from pytube.itags import get_format_profile
 
-
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 class Stream(object):
@@ -42,18 +42,18 @@ class Stream(object):
         # (Borg pattern).
         self._monostate = monostate
 
-        self.abr = None   # average bitrate (audio streams only)
-        self.fps = None   # frames per second (video streams only)
+        self.abr = None  # average bitrate (audio streams only)
+        self.fps = None  # frames per second (video streams only)
         self.itag = None  # stream format id (youtube nomenclature)
-        self.res = None   # resolution (e.g.: 480p, 720p, 1080p)
-        self.url = None   # signed download url
+        self.res = None  # resolution (e.g.: 480p, 720p, 1080p)
+        self.url = None  # signed download url
 
         self._filesize = None  # filesize in bytes
         self.mime_type = None  # content identifier (e.g.: video/mp4)
-        self.type = None       # the part of the mime before the slash
-        self.subtype = None    # the part of the mime after the slash
+        self.type = None  # the part of the mime before the slash
+        self.subtype = None  # the part of the mime after the slash
 
-        self.codecs = []         # audio/video encoders (e.g.: vp8, mp4a)
+        self.codecs = []  # audio/video encoders (e.g.: vp8, mp4a)
         self.audio_codec = None  # audio codec of the stream (e.g.: vorbis)
         self.video_codec = None  # video codec of the stream (e.g.: vp8)
 
@@ -206,11 +206,11 @@ class Stream(object):
         filename = filename or self.default_filename
 
         if filename_prefix:
-            filename = '{prefix}{filename}'\
+            filename = '{prefix}{filename}' \
                 .format(
-                    prefix=safe_filename(filename_prefix),
-                    filename=filename,
-                )
+                prefix=safe_filename(filename_prefix),
+                filename=filename,
+            )
 
         # file path
         fp = os.path.join(output_path, filename)
@@ -219,6 +219,8 @@ class Stream(object):
             'downloading (%s total bytes) file to %s',
             self.filesize, fp,
         )
+
+        logging.info("Downloading url: {}".format(self.url))
 
         with open(fp, 'wb') as fh:
             for chunk in request.get(self.url, streaming=True):
@@ -274,7 +276,7 @@ class Stream(object):
             'download progress\n%s',
             pprint.pformat(
                 {
-                    'chunk_size': len(chunk),
+                    'chunk_size'     : len(chunk),
                     'bytes_remaining': bytes_remaining,
                 }, indent=2,
             ),
