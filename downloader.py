8d05243fc7b12568514931991ef885a2cfa21240
JJ <jjisnow@gmail.com>
2018-07-23 18:24:56 +1000

    add previous redundant methods
    * moving to use the old downloader
    * setup file with correct callbacks

--- downloader.py
+++ downloader.py
@@ -44,7 +44,7 @@ def downloader():
 
     for file in arguments['URL']:
         logging.debug("Parsing url: {}".format(file))
-        yt = YouTube(file)
+        yt = YouTube(file, on_progress_callback=on_progress)
         parse_streams(yt.streams.all())
         if arguments['--list']:
             return 0
@@ -251,6 +251,80 @@ def download_file(download_target):
 
     logging.info("Final {} file: {}".format(download_target.type, fp))
     return fp
+
+
+def download_stream(download_target):
+    """Start downloading a YouTube stream. Based on original downloader
+
+    :param str url:
+        A valid YouTube watch URL.
+    :param str itag:
+        YouTube format identifier code.
+
+    """
+    print('\n{fn} | {fs} bytes'.format(
+        fn=download_target.default_filename,
+        fs=download_target.filesize,
+    ))
+    try:
+        download_target.download()
+        sys.stdout.write('\n')
+    except KeyboardInterrupt:
+        sys.exit()
+
+def get_terminal_size():
+    """Return the terminal size in rows and columns."""
+    rows, columns = os.popen('stty size', 'r').read().split()
+    return int(rows), int(columns)
+
+
+def display_progress_bar(bytes_received, filesize, ch='█', scale=0.55):
+    """Display a simple, pretty progress bar.
+
+    Example:
+    ~~~~~~~~
+    PSY - GANGNAM STYLE(강남스타일) MV.mp4
+    ↳ |███████████████████████████████████████| 100.0%
+
+    :param int bytes_received:
+        The delta between the total file size (bytes) and bytes already
+        written to disk.
+    :param int filesize:
+        File size of the media stream in bytes.
+    :param ch str:
+        Character to use for presenting progress segment.
+    :param float scale:
+        Scale multipler to reduce progress bar size.
+
+    """
+    _, columns = get_terminal_size()
+    max_width = int(columns * scale)
+
+    filled = int(round(max_width * bytes_received / float(filesize)))
+    remaining = max_width - filled
+    bar = ch * filled + ' ' * remaining
+    percent = round(100.0 * bytes_received / float(filesize), 1)
+    text = ' ↳ |{bar}| {percent}%\r'.format(bar=bar, percent=percent)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+def on_progress(stream, chunk, file_handle, bytes_remaining):
+    """On download progress callback function.
+
+    :param object stream:
+        An instance of :class:`Stream <Stream>` being downloaded.
+    :param file_handle:
+        The file handle where the media is being written to.
+    :type file_handle:
+        :py:class:`io.BufferedWriter`
+    :param int bytes_remaining:
+        How many bytes have been downloaded.
+
+    """
+    filesize = stream.filesize
+    bytes_received = filesize - bytes_remaining
+    display_progress_bar(bytes_received, filesize)
+
 
 
 def parse_arguments():
