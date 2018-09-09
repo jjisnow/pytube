cd6bcbf5f8c277b3725210993e9cab9821547b54
JJ <jjisnow@gmail.com>
2018-09-09 12:52:18 +1000

    move downloader() to correct location, add tests, comment long tests
    * added a few tests for the check_requirements function to check bad
    cases
    * commented long tests due to time delay in downloading

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 import pytest
 
+from pytube import downloader
+
 
 @pytest.fixture
 def base_command():
@@ -34,7 +36,7 @@ def captions():
     url = "https://www.youtube.com/watch?v=QRS8MkLhQmM"
 
     command = " ".join(("python",
-                        os.path.join("..", "downloader.py"),
+                        os.path.join("..", "pytube", "downloader.py"),
                         url,
                         "-v",
                         "-i"
@@ -53,98 +55,116 @@ def check_expected(base_command, downloaded_expected, itag, size_expected):
         os.remove(downloaded_expected)
         assert True
     else:
+        print()
         assert False
 
 
-def test_list(base_command):
-    assert subprocess.run(base_command + " 1 -l", shell=True)
+@pytest.mark.parametrize("args, expected", [
+    (('aria2c', 'ffmpeg', 'ffprobe'), None)
+])
+def test_downloader_check_requirements(args, expected):
+    assert downloader.check_requirements(*args) == None
 
 
-def test_audio(base_command):
-    # test for audio file only
-    downloaded_expected = Path("Short wildlife video clip HD-audio-output.mkv")
-    itag = 249
-    size_expected = 224096
-    check_expected(base_command, downloaded_expected, itag, size_expected)
+@pytest.mark.parametrize("args", [
+    ('blah_not_executable',)
+])
+def test_downloader_check_requirements_bad(args):
+    with pytest.raises(Exception):
+        downloader.check_requirements(args)
 
-
-def test_video_only(base_command):
-    # test for low quality video file muxing
-    downloaded_expected = Path("Short wildlife video clip HD-output.mkv")
-    itag = 278
-    size_expected = 843467
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_combined(base_command):
-    # test for low quality video file download
-    downloaded_expected = Path("Short wildlife video clip HD-output.mkv")
-    itag = 17
-    size_expected = 291297
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_hq_combined(base_command):
-    # test for high quality combined video file
-    downloaded_expected = Path("Short wildlife video clip HD-output.mkv")
-    itag = 247
-    size_expected = 6832831
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_audio_captions(captions):
-    # test for audio file only
-    base_command = captions
-    downloaded_expected = Path(
-        "YouTube Captions and Subtitles-audio-output.mkv")
-    itag = 249
-    size_expected = 550250
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_video_only_captions(captions):
-    # test for low quality video file muxing
-    base_command = captions
-    downloaded_expected = Path("YouTube Captions and Subtitles-output.mkv")
-    itag = 249
-    size_expected = 2603339
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_combined_captions(captions):
-    # test for low quality video file muxing
-    base_command = captions
-    downloaded_expected = Path("YouTube Captions and Subtitles-output.mkv")
-    if downloaded_expected.is_file():
-        os.remove(downloaded_expected)
-    itag = 17
-    size_expected = 1016003
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_hq_mux(captions):
-    # test for high quality combined video file
-    base_command = captions
-    downloaded_expected = Path("YouTube Captions and Subtitles-output.mkv")
-    itag = 243
-    size_expected = 4846884
-    check_expected(base_command, downloaded_expected, itag, size_expected)
-
-
-def test_non_safe_file_title():
-    url = "https://www.youtube.com/watch?v=BpaYqFd5S5c"
-    base_command = " ".join(("python",
-                        os.path.join("..", "downloader.py"),
-                        url,
-                        "-v",
-                        "-i"
-                        ))
-    itag = 278
-    cmd = base_command + " " + str(itag)
-    downloaded_expected = Path('Adam Savages New One Day Builds T-Shirt!-output.mkv')
-    if downloaded_expected.is_file():
-        os.remove(downloaded_expected)
-    exit_code = subprocess.run(cmd, shell=True)
-    os.remove(downloaded_expected)
-    print(exit_code.returncode)
-    assert exit_code.returncode == 0
+#
+# def test_list(base_command):
+#     assert subprocess.run(base_command + " 1 -l", shell=True)
+#
+#
+# def test_audio(base_command):
+#     # test for audio file only
+#     downloaded_expected = Path("Short wildlife video clip
+# HD-audio-output.mkv")
+#     itag = 249
+#     size_expected = 224096
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_video_only(base_command):
+#     # test for low quality video file muxing
+#     downloaded_expected = Path("Short wildlife video clip HD-output.mkv")
+#     itag = 278
+#     size_expected = 843467
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_combined(base_command):
+#     # test for low quality video file download
+#     downloaded_expected = Path("Short wildlife video clip HD-output.mkv")
+#     itag = 17
+#     size_expected = 291297
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_hq_combined(base_command):
+#     # test for high quality combined video file
+#     downloaded_expected = Path("Short wildlife video clip HD-output.mkv")
+#     itag = 247
+#     size_expected = 6832831
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_audio_captions(captions):
+#     # test for audio file only
+#     base_command = captions
+#     downloaded_expected = Path(
+#         "YouTube Captions and Subtitles-audio-output.mkv")
+#     itag = 249
+#     size_expected = 550250
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_video_only_captions(captions):
+#     # test for low quality video file muxing
+#     base_command = captions
+#     downloaded_expected = Path("YouTube Captions and
+# Subtitles-audio-output.mkv")
+#     itag = 249
+#     size_expected = 550250
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_combined_captions(captions):
+#     # test for low quality video file muxing
+#     base_command = captions
+#     downloaded_expected = Path("YouTube Captions and Subtitles-output.mkv")
+#     if downloaded_expected.is_file():
+#         os.remove(downloaded_expected)
+#     itag = 17
+#     size_expected = 1016003
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_hq_mux(captions):
+#     # test for high quality combined video file
+#     base_command = captions
+#     downloaded_expected = Path("YouTube Captions and Subtitles-output.mkv")
+#     itag = 243
+#     size_expected = 4846884
+#     check_expected(base_command, downloaded_expected, itag, size_expected)
+#
+#
+# def test_non_safe_file_title():
+#     url = "https://www.youtube.com/watch?v=BpaYqFd5S5c"
+#     base_command = " ".join(("python",
+#                         os.path.join("..", "downloader.py"),
+#                         url,
+#                         "-v",
+#                         "-i"
+#                         ))
+#     itag = 278
+#     cmd = base_command + " " + str(itag)
+#     downloaded_expected = Path('Adam Savages New One Day Builds
+# T-Shirt!-output.mkv')
+#     if downloaded_expected.is_file():
+#         os.remove(downloaded_expected)
+#     exit_code = subprocess.run(cmd, shell=True)
+#     os.remove(downloaded_expected)
+#     assert exit_code.returncode == 0
